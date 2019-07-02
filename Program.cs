6bb99bf042c3ffaf41f42a963754772cdc3dcdab
Shailesh Rathi <srathi@medline.com>
2019-07-02 19:21:15 +0530

    comment changes

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ namespace StringOperation
         {
             try
             {
-                // 1.look for the text1 which user enter.Ex.Type shailesh
-                //2.look for two occurences of text2 starting from postion after text1.Ex.type venkat
+                // 1.look for the text1 which user enter.Ex.Enter shailesh
+                //2.look for two occurences of text2 starting from postion after text1.Ex.Enter venkat
                 //3.skip 30th position from the second ocurences of text2 and read continues numbers
                 //4.read numeric and consider it as phone number, display informaton as if end with M then mobile, end with H -Home
 
